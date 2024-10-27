b1ce380075b6000e64067d237433203293d1382b
JackScottow <101928816+JackScottow@users.noreply.github.com>
2024-10-27 11:22:54 +0000

    infinite loading

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -1,22 +1,41 @@
 "use client";
 
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
 import Post from "@/components/posts/Post";
+import { Button } from "@/components/ui/button";
 import kyInstance from "@/lib/ky";
-import { PostData } from "@/lib/types";
+import { PostData, PostsPage } from "@/lib/types";
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
 
 const ForYouFeed = () => {
-  const query = useQuery<PostData[]>({
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
     queryKey: ["post-feed", "for-you"],
-    queryFn: kyInstance.get("/api/posts/for-you").json<PostData[]>,
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          "/api/posts/for-you",
+          pageParam ? { searchParams: { cursor: pageParam } } : {},
+        )
+        .json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
 
-  if (query.status === "pending") {
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+
+  if (status === "pending") {
     return <Loader2 className="mx-auto animate-spin" />;
   }
 
-  if (query.status === "error") {
+  if (status === "error") {
     return (
       <p className="text-center text-destructive">
         An error has occured whilst loading posts
@@ -25,11 +44,17 @@ const ForYouFeed = () => {
   }
 
   return (
-    <div className="space-y-5">
-      {query.data.map((post) => (
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
-    </div>
+
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+      {/* <Button onClick={() => fetchNextPage()}>Load More</Button> */}
+    </InfiniteScrollContainer>
   );
 };
 
